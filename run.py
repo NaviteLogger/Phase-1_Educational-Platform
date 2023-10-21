ee0423e06510fcd4597eff87ff0bd5214973a5bc
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-21 17:58:52 +0200

    Added a simple return of Hello World while vising the root page

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
+app = Flask(__name__)
+
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def hello_world():
+    return "Hello World!"

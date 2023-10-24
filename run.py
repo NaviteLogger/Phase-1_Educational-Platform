7f4a32a5b6f1bab37908e355e9ebced4dcc107ce
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 00:12:48 +0200

    Begun modifing the 'run.py' file: opted for the modular construction of def create_app

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    
+    app = Flask(__name__, instance_relative_config=True) # __name__ is the name of the current Python module

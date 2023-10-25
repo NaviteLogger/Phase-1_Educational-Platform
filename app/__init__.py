8b144a7ceff37d6a91b7b81281c81709fa59e53c
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 15:31:17 +0200

    Added the loading of environmental variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import FlaskLogin
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Initialize the extensions here, outside of the create_app function
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    # Default config
+    app.config.from_mapping(
+        SECRET_KEY=

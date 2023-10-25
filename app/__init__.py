04a72ecd12680236fca3e6bb8ecab2269541f1b7
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 15:32:33 +0200

    Added the loading of the test config if testing mode is selected

--- app/__init__.py
+++ app/__init__.py
@@ -16,4 +16,11 @@ def create_app(test_config=None):
 
     # Default config
     app.config.from_mapping(
-        SECRET_KEY=
+        SECRET_KEY=os.getenv('SECRET_KEY')
+    )
+
+    # Load the instance config, if it exists, when not testing
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)

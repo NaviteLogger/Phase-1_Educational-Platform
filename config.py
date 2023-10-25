7f8e8b9aa00240384cdeee23f8af07569bd01608
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 23:33:01 +0200

    Added more general content to config.py

--- config.py
+++ config.py
@@ -3,3 +3,14 @@ import os
 
 # The base directory of the project
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+# General config
+SECRET_KEY = os.urandom(32) # Generate a random secret key
+DEBUG = True
+
+# MySQL database config
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DB = os.getenv('MYSQL_DB')
+MYSQL_PORT = os.getenv('MYSQL_PORT')

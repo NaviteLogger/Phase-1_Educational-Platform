13a452f547ca58bb1270f02c203d6247733d8072
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 16:16:39 +0200

    Added the explanation of the 'application factory'

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,15 @@ load_dotenv()
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+'''
+The "Application Factory" (in this case the 'create_app' function) is a 
+pattern in Flask where the application is created inside a function,
+allowing for different instances of the app to be created with different 
+configurations. This is particularly useful for:
+- Configuring applications differently for different environments (development, testing, production, etc.)
+- Ensuring that resources (like databases) are correctly set up every time an app instance is created.
+- Facilitating unit testing by easily allowing the creation of a fresh app instance for each test.
+'''
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 

c419cb30e940d5107324ee952c8e94a4dea068f4
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 14:48:54 +0200

    Emptied the 'run.py' file

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-# This is the main file that will be used to run the application
-import os
-
-from flask import Flask
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True) # __name__ is the name of the current Python module
+'''
+This is the main file that will be used to run the application
+'''

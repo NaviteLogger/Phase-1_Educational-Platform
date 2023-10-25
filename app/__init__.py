859dea7513805a9b704ddcdbb85c6cb1b4fa326b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-25 15:26:20 +0200

    Added a 'create_app' function that will be used to initialize the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,6 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import FlaskLogin
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)

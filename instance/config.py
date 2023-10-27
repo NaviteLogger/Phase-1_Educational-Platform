84615dacd9a36680acf5ad33f0c1b6eba0dbca38
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-27 18:48:40 +0200

    Resoling RuntimeError: Either 'SQLALCHEMY_DATABASE_URI' or 'SQLALCHEMY_BINDS' must be set.

--- instance/config.py
+++ instance/config.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 class Config:
     # The base directory of the project
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    # Databace connection (common to all environments)
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_PORT = os.getenv('MYSQL_PORT')
     SECRET_KEY = os.getenv('DEFAULT_SECRET_KEY')
     DEBUG = False
     TESTING = False

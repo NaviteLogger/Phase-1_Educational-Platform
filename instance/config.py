ee70ca29be1d7f79ea6b508aed71b37770705a5d
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-27 16:07:43 +0200

    Trying to resolve the Werkzeug DoS: High resource usage error

--- instance/config.py
+++ instance/config.py
@@ -2,11 +2,20 @@ import os
 from dotenv import load_dotenv
 
 class Config:
+    # The base directory of the project
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    # Databace connection (common to all environments)
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_PORT = os.getenv('MYSQL_PORT')
     SECRET_KEY = os.getenv('DEFAULT_SECRET_KEY')
     DEBUG = False
     TESTING = False
 
 class DevelopmentConfig(Config):
+    # Development-specific configuration
+    MYSQL_DB = os.getenv('MYSQL_DB')
     SECRET_KEY = os.getenv('DEV_SECRET_KEY')
     DEBUG = True
     TESTING = True

423ddd1a7c468527a59f436b1c7d6de195b15749
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-27 15:50:54 +0200

    Added the environment-specific import of different secrety keys to 'config.py' file

--- instance/config.py
+++ instance/config.py
@@ -1,16 +1,22 @@
+import os
+from dotenv import load_dotenv
+
 class Config:
-    SECRET_KEY = 'default_secret_key'
+    SECRET_KEY = os.getenv('DEFAULT_SECRET_KEY')
     DEBUG = False
     TESTING = False
 
 class DevelopmentConfig(Config):
+    SECRET_KEY = os.getenv('DEV_SECRET_KEY')
     DEBUG = True
     TESTING = True
 
 class TestingConfig(Config):
+    SECRET_KEY = os.getenv('TEST_SECRET_KEY')
     DEBUG = True
     TESTING = True
 
 class ProductionConfig(Config):
+    SECRET_KEY = os.getenv('PROD_SECRET_KEY')
     DEBUG = False
     TESTING = False

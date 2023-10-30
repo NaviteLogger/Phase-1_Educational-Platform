9565b7685b40264417888b02c6e35f10b98293d4
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-30 22:37:43 +0100

    Added the handling of the GET request to the root URL

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from . import main
 
+# Handle GET requests for the root URL
 @main.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')

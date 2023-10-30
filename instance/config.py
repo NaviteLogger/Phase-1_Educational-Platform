e80ff1fe15a99bf0cf1074776fbe557c8805028a
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-30 13:53:13 +0100

    Moved the common content of 3 staging classes to the parent class Config

--- instance/config.py
+++ instance/config.py
@@ -8,6 +8,8 @@ class Config:
     # The base directory of the project
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = os.getenv('DEFAULT_SECRET_KEY')
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_PORT = os.getenv('MYSQL_PORT')
     DEBUG = False
     TESTING = False
 
@@ -16,8 +18,6 @@ class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = True
     DEV_SECRET_KEY = os.getenv('DEV_SECRET_KEY')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_PORT = os.getenv('MYSQL_PORT')
     MYSQL_PASSWORD_DEV = os.getenv('MYSQL_PASSWORD_DEV')
     MYSQL_DB_DEV = os.getenv('MYSQL_DB_DEV')
     MYSQL_USER_DEV = os.getenv('MYSQL_USER_DEV')
@@ -28,8 +28,6 @@ class TestingConfig(Config):
     DEBUG = True
     TESTING = True
     TEST_SECRET_KEY = os.getenv('TEST_SECRET_KEY')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_PORT = os.getenv('MYSQL_PORT')
     MYSQL_PASSWORD_TEST = os.getenv('MYSQL_PASSWORD_TEST')
     MYSQL_DB_TEST = os.getenv('MYSQL_DB_TEST')
     MYSQL_USER_TEST = os.getenv('MYSQL_USER_TEST')
@@ -40,8 +38,6 @@ class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
     PROD_SECRET_KEY = os.getenv('PROD_SECRET_KEY')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_PORT = os.getenv('MYSQL_PORT')
     MYSQL_PASSWORD_PROD = os.getenv('MYSQL_PASSWORD_PROD')
     MYSQL_DB_PROD = os.getenv('MYSQL_DB_PROD')
     MYSQL_USER_PROD = os.getenv('MYSQL_USER_PROD')

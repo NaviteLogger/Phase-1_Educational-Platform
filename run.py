1317fbab341048573835e4a96d7c22d40fad61c5
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-30 13:04:32 +0100

    Trying to resolve: RuntimeError: Either 'SQLALCHEMY_DATABASE_URI' or 'SQLALCHEMY_BINDS' must be set.

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 '''
 This is the main file that will be used to run the application
 '''
+from  app import create_app, db
+
+# Create the app instance
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()

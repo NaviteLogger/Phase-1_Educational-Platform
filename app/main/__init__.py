98204f08586cbe465fc4199258d5fcdca0d74e22
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-30 16:46:39 +0100

    Added the explanation of the usage of '__init__.py' file in each of the folders in the app folder

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,1 +1,12 @@
-# The __init__.py file is required to treat the main directory as a Python package.
+'''
+The __init__.py file is required to treat the main directory as a Python package.
+Also, the '__init__.py' file in each folder will typically be used to create a
+Blueprint for that particulare module. This allows us to define the routes specific
+to that module in the module's __init__.py file.
+'''
+
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+from . import routes

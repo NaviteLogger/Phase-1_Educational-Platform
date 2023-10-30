9dc8533d159195d2dff3337d2ac16211ffaf36ea
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-10-30 13:51:07 +0100

    Moved the content of TestingConfig into the TestingConfig class in'config.py' file

--- instance/config.py
+++ instance/config.py
@@ -27,6 +27,13 @@ class TestingConfig(Config):
     # Testing-specific configuration
     DEBUG = True
     TESTING = True
+    TEST_SECRET_KEY = os.getenv('TEST_SECRET_KEY')
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_PORT = os.getenv('MYSQL_PORT')
+    MYSQL_PASSWORD_TEST = os.getenv('MYSQL_PASSWORD_TEST')
+    MYSQL_DB_TEST = os.getenv('MYSQL_DB_TEST')
+    MYSQL_USER_TEST = os.getenv('MYSQL_USER_TEST')
+    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER_TEST}:{MYSQL_PASSWORD_TEST}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB_TEST}?ssl-mode=REQUIRED'
 
 class ProductionConfig(Config):
     # Production-specific configuration

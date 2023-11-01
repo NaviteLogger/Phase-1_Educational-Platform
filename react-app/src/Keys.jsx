e5799b18cfd070e178881129464fc600bc7609b3
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-11-01 13:51:09 +0100

    Added code explaining the concept of 'default props'

--- react-app/src/Keys.jsx
+++ react-app/src/Keys.jsx
@@ -54,3 +54,29 @@ export default function App() {
     </div>
   );
 }
+
+// Default Props
+function Button({ text, color, fontSize }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px"
+  };
+
+  return <button style={buttonStyle}>{text}</button>;
+}
+
+Button.defaultProps = {
+  text: "Click Me!",
+  color: "blue",
+  fontSize: 12
+};
+
+export default function App() {
+  return (
+    <div>
+      <Button />
+      <Button text="Don't Click Me!" color="red" />
+      <Button fontSize={20} />
+    </div>
+  );
+}

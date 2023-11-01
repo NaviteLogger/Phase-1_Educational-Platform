3ca8fa63b61e2f8a4a456e9bb2d334777a396adf
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-11-01 13:44:56 +0100

    Added more sample code explaining the 'props' concept

--- react-app/src/Keys.jsx
+++ react-app/src/Keys.jsx
@@ -18,11 +18,31 @@ function TodoList() {
 
 function Button(props) {
   const buttonStyle = {
-    color: props.color,
+    color: props.color, //props.propertyName
     fontSize: props.fontSize + "px",
   };
 
   return <button style={buttonStyle}>{props.text}</button>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <Button text="Click Me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
+    </div>
+  );
+}
+
+// Props destructuring
+function Button({ text, color, fontSize }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px"
+  };
+
+  return <button style={buttonStyle}>{text}</button>;
 }
 
 export default function App() {

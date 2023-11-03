9048992a3d7eca3d67ada1011aa85e553291c39f
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-11-03 23:49:03 +0100

    Added some more sample content to the React Class

--- react-app/src/Classes.jsx
+++ react-app/src/Classes.jsx
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
 
-class ClassInput extends Component {
-    //Some code goes here
 
-    //Each class requires a constructor
+class ClassInput extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+
+        this.state = {
+            todos: [],
+            inputVal: "",
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    //Each class requires a render function
+    handleInputChange(e) {
+        this.setState((state) => ({
+            ...state,
+            inputVal: e.target.value,
+        }));
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState((state) => ({
+            todos: state.todos.concat(state.inputVal),
+            inputVal: "",
+        }));
+    }
+
     render() {
         return (
-            <div>
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                <p>{this.state.value}</p>
-            </div>
+            <section>
+                <h3>{this.props.name}</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="task-entry">Enter a task: </label>
+                    <input
+                        type="text"
+                        name="task-entry"
+                        value={this.state.inputVal}
+                        onChange={this.handleInputChange}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+                <h4>All the tasks!</h4>
+                <ul>
+                    {this.state.todos.map((todo) => (
+                        <li key={todo}>{todo}</li>
+                    ))}
+                </ul>
+            </section>
         );
     }
 }
+
 
 export default ClassInput;

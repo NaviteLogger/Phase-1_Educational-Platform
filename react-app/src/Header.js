f88364ab01fcbf418add8dac8c545c59238ae434
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-11-07 23:26:46 +0100

    Added sample nav buttons to the middle section of the header

--- react-app/src/Header.js
+++ react-app/src/Header.js
@@ -9,6 +9,10 @@ function Header() {
                 <img src='' alt='logo' />
             </div>
             <div className='middleBox'>
+                <nav>Strona Główna</nav>
+                <nav>Oferta</nav>
+                <nav>Kontakt</nav>
+                <nav>O nas</nav>
             </div>
             <div className='rightBox'></div>
 

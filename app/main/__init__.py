d3b3e2037849ec4b1641648910d0138916af21ce
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-12-11 23:03:57 +0100

    Added the minor change

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 The __init__.py file is required to treat the main directory as a Python package.
 This means that the application directory can be treated as a package, and therefore
 the modules cam be imported into the __init__.py file in the 'app' folder. This is done 
@@ -7,15 +7,18 @@ in the __init__.py file in the 'app' folder by the following line: 'from app.mai
 Also, the '__init__.py' file in each folder will typically be used to create a
 Blueprint for that particulare module. This allows us to define the routes specific
 to that module in the module's __init__.py file.
-'''
+"""
 
 from flask import Blueprint
 
-'''
+"""
 This creates a Blueprint named 'main'. Like the application object, the blueprint 
-needs to know where it’s defined, so __name__ is passed as the second argument.
-'''
-main = Blueprint('main', __name__,)
+needs to know where it's defined, so __name__ is passed as the second argument.
+"""
+main = Blueprint(
+    "main",
+    __name__,
+)
 
 # Import the routes module from the 'routes.py' file located in the same directory as this __init__.py file.
 from . import routes
